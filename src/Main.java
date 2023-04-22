271cc3fda1cdf195fb8c9d88c2bda1335c324f88
helga <helga-nikol09@yandex.ru>
2023-04-22 23:11:18 +0300

    1 part

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        task1 ();
+    }
+    public static void task1 () {
+        System.out.println("Задача1");
+        byte apple = 8;
+        System.out.println("Значение переменной apple с типом byte равно " + apple );
+        short fruits = 274;
+        System.out.println("Значение переменной fruits с типом  short равно " + fruits);
+        int garden = 493278;
+        System.out.println("Значение переменной garden с типом int равно " + garden);
+        long planet = 345629245;
+        System.out.println("Знаечение переменной planet с типом long равно " + planet);
+        float basketWeight = 6;
+        System.out.println("Значение переменной basketWeight c типом float равно " + basketWeight);
+        double boxWeight = 10.9616;
+        System.out.print("Значение переменной boxWeight с типом double равно " + boxWeight);
+    }
+}
