9eb54cc415fda9fc03e2d5cecd9b777c14042dee
helga <helga-nikol09@yandex.ru>
2023-04-23 23:48:48 +0300

    1 part

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         task1 ();
+        task2 ();
+        task3 ();
     }
     public static void task1 () {
         System.out.println("Задача1");
@@ -16,5 +18,25 @@ public class Main {
         System.out.println("Значение переменной basketWeight c типом float равно " + basketWeight);
         double boxWeight = 10.9616;
         System.out.print("Значение переменной boxWeight с типом double равно " + boxWeight);
+    }
+    public static void task2 () {
+        System.out.println("Задача2");
+        float one = 27.12f;
+        long two = 987678965549L;
+        double three = 2.786;
+        short four = 569;
+        short five = 159;
+        int six = 27897;
+        byte seven = 67;
+    }
+    public static void task3 () {
+        System.out.println("Задача3");
+        byte lyudmilaPavlovna = 23;
+        byte annaSergeevna = 27;
+        byte ekaterinaAndreevna = 30;
+        int totalStudents = lyudmilaPavlovna + annaSergeevna + ekaterinaAndreevna;
+        short totalSheels = 480;
+        float sheetStudent1 = totalSheels / totalStudents;
+        System.out.println("На каждого ученика рассчитано " + sheetStudent1 + " листов бумаги");
     }
 }
